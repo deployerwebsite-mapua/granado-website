bf22567917933e3b637880eb9facdb1359458c22
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2026-02-21 20:44:54 +0800

    Initial Front-page Code

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,45 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <div>
+        <header id="hero-container">
+          <div id="hero-content" className="relative top-32 inset-0 flex flex-col items-center justify-center text-white">
+            <h1 className="relative z-20 text-3xl font-serif tracking-widest uppercase">Gil Granado</h1>
+            <p className="mt-4 tracking-[0.4em] uppercase text-xs opacity-90">Haute Couture</p>
+          </div>
+          <Image
+              src="/background.jpg"
+              alt="Background"
+              width={800}
+              height={200}
+              priority
+              className="absolute top-0 left-0 z-10"
+          />
+        <div className="relative flex flex-row z-15">
+          <Button>
+            Log-in
+          </Button>
+          <Button>
+            Sign-up
+          </Button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        </header>
+      </div>
+      <div className="flex flex-row bg-[#00170f] border-2 ">
+        <div className="flex flex-column">
+          <div className="text-[#E5DED0]">WORKS AND DESIGN</div>
+          <div className="text-[#E5DED0]">A masterpiece of Arabian-inspired luxury, this gown flows like liquid fire in a deep, commanding red.
+            Adorned with shimmering crystals and intricate gold embroidery, it captures the regal mystery of a desert midnight.
+            It is a silhouette of pure power and dazzling elegance, designed for an unforgettable entrance.</div>
+          <Button>
+            View More
+          </Button>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
